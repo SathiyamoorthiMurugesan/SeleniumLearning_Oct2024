e1a4b45f644a7b63408e7cc9acbb8b351f985a0e
SathiyamoorthiMurugesan <sathya08ece@gmail.com>
2024-11-18 22:52:12 +0530

    For loop class 2 and Switch condition/case

--- src/main/java/forLoopsInJava/ForLoopCheck.java
+++ src/main/java/forLoopsInJava/ForLoopCheck.java
@@ -10,13 +10,13 @@ public class ForLoopCheck {
 //		method1();
 //		method1();
 //		method1();
-
+//
 //		for(int i = 1; i<=5; i++) {
 //			System.out.println(i);
 //			method1();
 //		}
 //		System.out.println(i);
-
+//
 //		int i;
 //		for (i = 1; i <= 5; i++) {
 ////			System.out.println(i);
@@ -36,30 +36,30 @@ public class ForLoopCheck {
 //		for (number = 0; number <5; number++) {
 //			method1();
 //		}
-
+//
 //		for (number = 65; number >60; number--) {
 //			method1();
 //		}
-
+//
 //		for (int i = 1; i <= 5; i++) {
 //			method1();
 //			for (int j = 1; j <= 5; j++) {
 //				method2();
 //			}
 //		}
-
+//
 //		for (int i = 1; i <= 5; i++) {
 //			if(i!=2)
 //			method1();
 //		}
-
+//
 //		for (int i = 1; i <= 5; i++) {
 //			if(i==4) {
 //			break;
 //			}
 //			method1();
 //		}
-
+//
 //		for (int i = 1; i <= 5; i++) {
 //			System.out.println("i value is "+i);
 //			for (int j = 1; j <= 5; j++) {
@@ -69,7 +69,7 @@ public class ForLoopCheck {
 //				System.out.println("***j value is "+j);
 //			}
 //		}
-
+//
 //		for (int i = 1; i <= 5; i++) {
 //			if (i == 2) {
 //				continue;
@@ -88,31 +88,86 @@ public class ForLoopCheck {
 //			}
 //			System.out.println("Hii");
 //		}
-
+//
 //		for (int i = 1; i <= 5; i++) {
 //			System.out.println("i value is " + i);
 //			for (int j = 1; j <= 5; j++) {
 //				System.out.println("***j value is " + j + "****");
 //			}
 //		}
-
-		for (int i = 1; i <= 5; i++) {
-
-			for (int j = 1; j <= 5; j++) {
-//				System.out.println("*");
-//				System.out.print("*"); //*****
-				System.out.print("* "); //* * * * * 
-				
-			}
-			System.out.println();
-//			System.out.print();
-		}
-		
+//
+//		for (int i = 1; i <= 5; i++) {
+//
+//			for (int j = 1; j <= 5; j++) {
+////				System.out.println("*");
+////				System.out.print("*"); //*****
+//				System.out.print("* "); //* * * * * 
+//				
+//			}
+//			System.out.println();
+////			System.out.print();
+//		}
+//		
 //		System.out.print("Hiii");
 //		System.out.println("***");
 //		System.out.println("Hello");
+//
+//		for (int i = 1; i <= 5; i++) {
+//
+//			for (int j = 1; j <= 5; j++) {
+//				System.out.print("* "); //* * * * * 
+//			}
+//			System.out.println();
+//		}
+//		
+//		for (int i = 1; i <= 5; i++) {
+//
+//			for (int j = 1; j <= 5; j++) {
+////				if(i<j) {
+//					if(j>i) {
+//					break;
+//				}
+//				System.out.print("* "); 
+//			}
+//			System.out.println();
+//		}
+//		
+//		for (int i = 1; i <= 5; i++) {
+//
+//			for (int j = 1; j <=i; j++) {
+//				System.out.print("* "); 
+//			}
+//			System.out.println();
+//		}
+//		
+//		for (int i = 1; i <= 5; i++) {
+//
+//			for (int j = 1; j <= i; j++) {
+//				System.out.print(j+" "); 
+//			}
+//			System.out.println();
+//		}
+//		
+//		for (int i = 1; i <= 5; i++) {
+//
+//			for (int j = 1; j <= i; j++) {
+//				System.out.print(i+" "); 
+//			}
+//			System.out.println();
+//		}
+		
+		for (int i = 1; i <= 5; i++) {
 
+			for (int j = 1; j <= 5; j++) {
+				if(i==j) 
+					System.out.print("# ");
+				else
+				System.out.print(j+" "); 
+			}
+			System.out.println();
+		}
 	}
+	
 
 	public static void method1() {
 		System.out.println("Hii");
